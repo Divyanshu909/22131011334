bed06ab5086d7e36af3fa02f7af87708acbb8894
Divyanshu909 <119447079+Divyanshu909@users.noreply.github.com>
2025-06-27 15:23:39 +0530

    Update App.js

--- Frontend-Test-Submission/src/App.js
+++ Frontend-Test-Submission/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import routes from './routes';
 import { logEvent } from './logging-middleware/logger';
+import './logging-middleware/logger.test';
 
 // Component to log route changes
 const RouteChangeLogger = () => {
