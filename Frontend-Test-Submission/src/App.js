68c5f1247e46a5a47adebd8257a11802c6325374
Divyanshu909 <119447079+Divyanshu909@users.noreply.github.com>
2025-06-27 15:19:10 +0530

    Update App.js

--- Frontend-Test-Submission/src/App.js
+++ Frontend-Test-Submission/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import routes from './routes';
+import { logEvent } from './logging-middleware/logger';
+
+// Component to log route changes
+const RouteChangeLogger = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    logEvent("Route Visit", { path: location.pathname });
+  }, [location]);
+
+  return null;
+};
 
 function App() {
   return (
     <BrowserRouter>
+      <RouteChangeLogger /> {/* Logs every route change */}
       <Routes>
         {routes.map((route, idx) => (
           <Route key={idx} path={route.path} element={route.element} />
